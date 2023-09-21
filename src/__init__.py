bd4581acacae05724cde569547705268069bea1c
Chetane <chetane1005@gmail.com>
2023-09-21 11:05:43 +0400

    Nineth Commit

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# calculator/__init__.py
+"""Do math with your own functions.
+Modules exported by this package:
+- `calculations`: Provide several sample math calculations.
+"""
--- src/calculator.py
+++ src/calculator.py
@@ -1,13 +1,32 @@
 # calculator/calculations.py
-def add(a, b):
-    """Compute and return the sum of two numbers.
-Args:
-a (float): A number representing the first addend in the addition.
-b (float): A number representing the second addend in the addition.
-Returns:
-float: A number representing the arithmetic sum of `a` and `b`.
+"""Provide several sample math calculations.
+6
+This module allows the user to make mathematical calculations.
+The module contains the following functions:
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
 """
+
+from typing import Union
+def add(a: Union[float, int], b: Union[float, int]) -> float:
+
+    """Compute and return the sum of two numbers.
+    Examples:
+    >>> add(4.0, 2.0)
+    6.0
+    >>> add(4, 2)
+    6.0
+    Args:
+    a: A number representing the first addend in the addition.
+    b: A number representing the second addend in the addition.
+    Returns:
+    A number representing the arithmetic sum of `a` and `b`.
+    """
+
     return float(a + b)
+
 
 def subtract(a, b):
     return float(a - b)
